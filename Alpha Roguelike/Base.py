4939df1da42a21f12494b7d50f622d4ce02031c5
friedice <58054670+friedice5467@users.noreply.github.com>
2020-03-09 14:51:17 -0700

    Update Base.py
    Added basic combat, basic AI, and basic deaths

--- Alpha Roguelike/Base.py
+++ Alpha Roguelike/Base.py
@@ -1,6 +1,9 @@
 import tdl
 from random import randint
+import math
 import colors
+import combat_prop
+import AI
 
 #windowed size
 SCREEN_WIDTH = 80
@@ -63,19 +66,44 @@ class Rect:
 class GameObject:
     #this is a generic object: the player, a monster, an item, the stairs...
     #it's always represented by a character on screen.
-    def __init__(self, x, y, char, name, color, blocks=False):
+    def __init__(self, x, y, char, name, color, blocks=False, fighter= None, ai= None):
         self.x = x
         self.y = y
         self.char = char
         self.color = color
         self.name = name
         self.blocks = blocks
+        self.fighter = fighter
+        if self.fighter:  #let the fighter component know who owns it
+            self.fighter.owner = self
+ 
+        self.ai = ai
+        if self.ai:  #let the AI component know who owns it
+            self.ai.owner = self
  
     def move(self, dx, dy):
         #move by the given amount, if the destination is not blocked
         if not is_blocked(self.x + dx, self.y + dy):
             self.x += dx
             self.y += dy
+
+    def move_towards(self, target_x, target_y):
+        #vector from this object to the target, and distance; AI movement
+        dx = target_x - self.x
+        dy = target_y - self.y
+        distance = math.sqrt(dx ** 2 + dy ** 2)
+ 
+        #normalize it to length 1 (preserving direction), then round it and
+        #convert to integer so the movement is restricted to the map grid
+        dx = int(round(dx / distance))
+        dy = int(round(dy / distance))
+        self.move(dx, dy)
+
+    def distance_to(self, other):
+        #return the distance to another object
+        dx = other.x - self.x
+        dy = other.y - self.y
+        return math.sqrt(dx ** 2 + dy ** 2)
  
     def draw(self):
         global visible_tiles
@@ -84,11 +112,68 @@ class GameObject:
         if (self.x, self.y) in visible_tiles:
             #draw the character that represents this object at its position
             con.draw_char(self.x, self.y, self.char, self.color, bg=None)
+
+    def send_to_back(self):
+        #make this object be drawn first, so all others appear above it if they're in the same tile.
+        global objects
+        objects.remove(self)
+        objects.insert(0, self)
  
     def clear(self):
         #erase the character that represents this object
         con.draw_char(self.x, self.y, ' ', self.color, bg=None)
  
+class Fighter:
+    #combat-related properties and methods (monster, player, NPC).
+    def __init__(self, hp, mp, defense, power, agility, death_function=None):
+        self.max_hp = hp
+        self.max_mp = mp
+        self.hp = hp
+        self.mp = mp
+        self.defense = defense
+        self.power = power
+        self.agility = agility
+        self.death_function = death_function
+
+    def take_damage(self, damage):
+        #apply damage if possible
+        if damage > 0:
+            self.hp -= damage
+ 
+            #check for death. if there's a death function, call it
+            if self.hp <= 0:
+                function = self.death_function
+                if function is not None:
+                    function(self.owner)
+ 
+    def attack(self, target):
+        #a simple formula for attack damage
+        damage = self.power - target.fighter.defense
+ 
+        if damage > 0:
+            #make the target take some damage
+            print(self.owner.name.capitalize() + ' attacks ' + target.name + 
+                  ' for ' + str(damage) + ' hit points.')
+            target.fighter.take_damage(damage)
+        else:
+            print(self.owner.name.capitalize() + ' attacks ' + target.name + 
+                  ' but it has no effect!')
+
+class BasicMonster:
+    #AI for a basic monster.
+    def take_turn(self):
+        #a basic monster takes its turn. If you can see it, it can see you
+        monster = self.owner
+        if (monster.x, monster.y) in visible_tiles:
+ 
+            #move towards player if far away
+            if monster.distance_to(player) >= 2:
+                monster.move_towards(player.x, player.y)
+ 
+            #close enough, attack! (if the player is still alive.)
+            elif player.fighter.hp > 0:
+                monster.fighter.attack(player)
+
 def is_blocked(x, y):
     #first test the map tile
     if my_map[x][y].blocked:
@@ -218,28 +303,60 @@ def place_objects(room):
         if not is_blocked(x, y):
             if choice < 25: 
                 #create an orc at 25% chance
-                monster = GameObject(x,y, 'o', 'orc', colors.desaturated_green, blocks=True)
+                fighter_component = Fighter(hp=10, mp=5, defense=0, power=3, agility= 1, death_function=monster_death)
+                ai_component = BasicMonster()
+
+                monster = GameObject(x,y, 'o', 'orc', colors.desaturated_green, 
+                                    blocks=True, fighter=fighter_component, ai=ai_component)
             elif choice < 25+50:
                 #create a goblin at 50% chance
-                monster = GameObject(x, y, 'g', 'goblin', colors.light_green, blocks=True)
+                fighter_component = Fighter(hp=4, mp=5, defense=0, power=2, agility= 2, death_function=monster_death)
+                ai_component = BasicMonster()
+
+                monster = GameObject(x, y, 'g', 'goblin', colors.light_green, 
+                                    blocks=True, fighter=fighter_component, ai=ai_component)
             elif choice < 25+50+5:
                 #create a uruk at 5% chance
-                monster = GameObject(x, y, 'u', 'uruk', colors.green, blocks=True)
+                fighter_component = Fighter(hp=15, mp=5, defense=1, power=3, agility= 1, death_function=monster_death)
+                ai_component = BasicMonster()
+
+                monster = GameObject(x, y, 'u', 'uruk', colors.green, 
+                                    blocks=True, fighter=fighter_component, ai=ai_component)
             elif choice < 25+50+5+10:
                 #create a greater goblin at 10% chance
-                monster = GameObject(x, y, 'G', 'greater goblin', colors.light_green, blocks=True)
+                fighter_component = Fighter(hp=8, mp=5, defense=0, power=3, agility= 2, death_function=monster_death)
+                ai_component = BasicMonster()
+
+                monster = GameObject(x, y, 'G', 'greater goblin', colors.light_green,
+                                    blocks=True, fighter=fighter_component, ai=ai_component)
             elif choice < 25+50+5+10+5:
                 #create a ogre at 5% chance
-                monster = GameObject(x, y, 'r', 'ogre', colors.desaturated_amber, blocks=True)
+                fighter_component = Fighter(hp=12, mp=5, defense=2, power=3, agility= 1, death_function=monster_death)
+                ai_component = BasicMonster()
+
+                monster = GameObject(x, y, 'r', 'ogre', colors.desaturated_amber, 
+                                    blocks=True, fighter=fighter_component, ai=ai_component)
             elif choice < 25+50+5+10+5+2:
                 #create a cyclops at 2% chance
-                monster = GameObject(x, y, 'c', 'cyclops', colors.desaturated_yellow, blocks=True)
+                fighter_component = Fighter(hp=15, mp=5, defense=1, power=4, agility= 1, death_function=monster_death)
+                ai_component = BasicMonster()
+
+                monster = GameObject(x, y, 'c', 'cyclops', colors.desaturated_yellow, 
+                                    blocks=True, fighter=fighter_component, ai=ai_component)
             elif choice < 25+50+5+10+5+2:
                 #create a troll at 2% chance
-                monster = GameObject(x, y, 'T', 'Troll', colors.darkest_green, blocks=True)
+                fighter_component = Fighter(hp=16, mp=5, defense=1, power=4, agility= 1, death_function=monster_death)
+                ai_component = BasicMonster()
+
+                monster = GameObject(x, y, 'T', 'Troll', colors.darkest_green, 
+                                    blocks=True, fighter=fighter_component, ai=ai_component)
             else:
                 #create a dragon at 1% chance
-                monster = GameObject(x, y, 'D', 'Dragon', colors.red, blocks=True)
+                fighter_component = Fighter(hp=30, mp=5, defense=3, power=5, agility= 3, death_function=monster_death)
+                ai_component = BasicMonster()
+
+                monster = GameObject(x, y, 'D', 'Dragon', colors.red, 
+                                    blocks=True, fighter=fighter_component, ai=ai_component)
 
             objects.append(monster)
 
@@ -279,12 +396,20 @@ def render_all():
                         #since visible, explore
                     my_map[x][y].explored = True
  
-    #draw all objects in the list
+    #draw all objects in the list, except the player. we want it to
+    #always appear over all other objects! so it's drawn later.
     for obj in objects:
-        obj.draw()
+        if obj != player:
+            obj.draw()
+    player.draw()
  
     #blit the contents of "con" to the root console and present it
     root.blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0)
+
+    #show the player's stats
+    #show the player's stats
+    con.draw_str(1, SCREEN_HEIGHT - 2, 'HP: ' + str(player.fighter.hp) + '/' + 
+                 str(player.fighter.max_hp) + ' ')
 
 def is_blocked(x, y):
     #first test the map tile
@@ -308,13 +433,13 @@ def player_move_or_attack(dx, dy):
     #try to find an attackable object there
     target = None
     for obj in objects:
-        if obj.x == x and obj.y == y:
+        if obj.fighter and obj.x == x and obj.y == y:
             target = obj
             break
  
     #attack if target found, move otherwise
     if target is not None:
-        print('The ' + target.name + ' laughs at your puny efforts to attack it!')
+        player.fighter.attack(target)
     else:
         player.move(dx, dy)
         fov_recompute = True
@@ -353,36 +478,20 @@ def handle_keys():
         #movement keys turnbased
         if user_input.key == 'UP':
             player_move_or_attack(0, -1)
-            
-
         elif user_input.key == 'KP8':
             player_move_or_attack(0, -1)
-            
-
         elif user_input.key == 'DOWN' :
             player_move_or_attack(0, 1)
-            
-
         elif user_input.key == 'KP2' :
             player_move_or_attack(0, 1)
-            
-
         elif user_input.key == 'LEFT' :
             player_move_or_attack(-1, 0)
-            
-
         elif user_input.key == 'KP4' :
             player_move_or_attack(-1, 0)
-            
-
         elif user_input.key == 'RIGHT' :
             player_move_or_attack(1,0)
-            
-
         elif user_input.key == 'KP6' :
             player_move_or_attack(1,0)
-            
-
         #diagonal movement
         elif user_input.key == 'KP9':
             player_move_or_attack(1,-1)
@@ -400,6 +509,28 @@ def handle_keys():
             return 'didnt-take-turn'
 
 
+def player_death(player):
+    #the game ended!
+    global game_state
+    print('You died!')
+    game_state = 'dead'
+ 
+    #for added effect, transform the player into a corpse!
+    player.char = '%'
+    player.color = colors.dark_red
+ 
+def monster_death(monster):
+    #transform it into a nasty corpse! it doesn't block, can't be
+    #attacked and doesn't move
+    print(monster.name.capitalize() + ' is dead!')
+    monster.char = '%'
+    monster.color = colors.dark_red
+    monster.blocks = False
+    monster.fighter = None
+    monster.ai = None
+    monster.name = 'remains of ' + monster.name
+    monster.send_to_back()
+
 #############################################
 # Initialization & Main Loop                #
 #############################################
@@ -411,7 +542,8 @@ con = tdl.Console(SCREEN_WIDTH, SCREEN_HEIGHT)
 
 
 #create object representing the player
-player = GameObject(0, 0, '@', 'player', colors.white, blocks=True)
+fighter_component = Fighter(hp=30, mp=10, defense=2, power=5, agility=2, death_function=player_death)
+player = GameObject(0, 0, '@', 'player', colors.white, blocks=True, fighter=fighter_component)
  
 #the list of objects starting with the player
 objects = [player]
@@ -439,3 +571,10 @@ while not tdl.event.is_window_closed():
     player_action = handle_keys()
     if player_action == 'exit':
         break
+
+    if game_state == 'playing' and player_action != 'didnt-take-turn':
+        for obj in objects:
+            if obj.ai:
+                obj.ai.take_turn()
+ 
+
