c1911f26a2e4de2448de292e1a7751d4d95fc02e
friedice <58054670+friedice5467@users.noreply.github.com>
2020-03-22 10:33:58 -0700

    Update entity.py
    added movetowards and distance to using math functions

--- Alpha Roguelike/entity.py
+++ Alpha Roguelike/entity.py
@@ -1,4 +1,4 @@
-
+import math
 
 class Entity:
     """
@@ -25,6 +25,23 @@ class Entity:
         #positions the entity by given amount
         self.x += dx
         self.y += dy
+
+    def move_towards(self, target_x, target_y, game_map, entities):
+        dx = target_x - self.x
+        dy = target_y - self.y
+        distance = math.sqrt(dx ** 2 + dy ** 2)
+
+        dx = int(round(dx / distance))
+        dy = int(round(dy / distance))
+
+        if not (game_map.is_blocked(self.x + dx, self.y + dy) or
+                    get_blocking_entities_at_location(entities, self.x + dx, self.y + dy)):
+            self.move(dx, dy)
+
+    def distance_to(self, other):
+        dx = other.x - self.x
+        dy = other.y - self.y
+        return math.sqrt(dx ** 2 + dy ** 2)
 
 def get_blocking_entities_at_location(entities, destination_x, destination_y):
     for entity in entities:
