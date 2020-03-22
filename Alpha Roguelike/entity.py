a23f5222dbbd0d57be82ea2becff071893e13063
friedice <58054670+friedice5467@users.noreply.github.com>
2020-03-22 10:26:55 -0700

    Update entity.py
    updated entity to include ai and fighter class

--- Alpha Roguelike/entity.py
+++ Alpha Roguelike/entity.py
@@ -5,13 +5,21 @@ class Entity:
     A generic object to represent x=, y=, char=, color=, name=, blocks=            
     """
 
-    def __init__(self, x, y, char, color, name, blocks=False):
+    def __init__(self, x, y, char, color, name, blocks=False, fighter=None, ai=None):
         self.x = x
         self.y = y
         self.char = char
         self.color = color
         self.name = name
         self.blocks = blocks
+        self.fighter = fighter
+        self.ai = ai
+
+        if self.fighter:
+            self.fighter.owner = self
+
+        if self.ai:
+            self.ai.owner = self
     
     def move (self, dx, dy):
         #positions the entity by given amount
