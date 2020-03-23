308f8db18957f4978d52299ff448a95df2f51fbb
friedice <58054670+friedice5467@users.noreply.github.com>
2020-03-22 22:10:11 -0700

    Update render_function.py
    3/22/2020
    cleans up corpse and rendering priority

--- Alpha Roguelike/render_function.py
+++ Alpha Roguelike/render_function.py
@@ -1,7 +1,17 @@
 import tcod as libtcod
 
-def render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, screen_height, colors):
+from enum import Enum
+
+
+class RenderOrder(Enum):
+    CORPSE = 1
+    ITEM = 2
+    ACTOR = 3
+
+
+def render_all(con, entities, player, game_map, fov_map, fov_recompute, screen_width, screen_height, colors):
     if fov_recompute:
+    # Draw all the tiles in the game map
         for y in range(game_map.height):
             for x in range(game_map.width):
                 visible = libtcod.map_is_in_fov(fov_map, x, y)
@@ -14,30 +24,35 @@ def render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, sc
                         libtcod.console_set_char_background(con, x, y, colors.get('light_ground'), libtcod.BKGND_SET)
 
                     game_map.tiles[x][y].explored = True
-
                 elif game_map.tiles[x][y].explored:
                     if wall:
                         libtcod.console_set_char_background(con, x, y, colors.get('dark_wall'), libtcod.BKGND_SET)
                     else:
                         libtcod.console_set_char_background(con, x, y, colors.get('dark_ground'), libtcod.BKGND_SET)
 
+    entities_in_render_order = sorted(entities, key=lambda x: x.render_order.value)
 
-    #draw all entities in the list
-    for entity in entities:
+    # Draw all entities in the list
+    for entity in entities_in_render_order:
         draw_entity(con, entity, fov_map)
 
-    libtcod.console_blit(src=con,x=0,y=0,w=screen_width,h=screen_height,dst=0,xdst=0 ,ydst=0)
+    libtcod.console_set_default_foreground(con, libtcod.white)
+    libtcod.console_print_ex(con, 1, screen_height - 2, libtcod.BKGND_NONE, libtcod.LEFT,
+                             'HP: {0:02}/{1:02}'.format(player.fighter.hp, player.fighter.max_hp))
+
+    libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
+
 
 def clear_all(con, entities):
-    #loop that calls clear entity
     for entity in entities:
         clear_entity(con, entity)
 
 def draw_entity(con, entity, fov_map):
     if libtcod.map_is_in_fov(fov_map, entity.x, entity.y):
         libtcod.console_set_default_foreground(con, entity.color)
-        libtcod.console_put_char(con=con, x=entity.x, y=entity.y, c=entity.char, flag=libtcod.BKGND_NONE)
+        libtcod.console_put_char(con, entity.x, entity.y, entity.char, libtcod.BKGND_NONE)
+
 
 def clear_entity(con, entity):
-    #erase the character that represents this object
-    libtcod.console_put_char(con=con, x=entity.x, y=entity.y, c=' ', flag=libtcod.BKGND_NONE)
+    # erase the character that represents this object
+    libtcod.console_put_char(con, entity.x, entity.y, ' ', libtcod.BKGND_NONE)
