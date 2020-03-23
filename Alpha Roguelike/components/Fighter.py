1a61571f2a057ba6b2cd4b6adfe14ac1691b0666
friedice <58054670+friedice5467@users.noreply.github.com>
2020-03-22 23:17:55 -0700

    Update Fighter.py
    added stamina and mana to fighter class
    updated fighter class to append messages

--- Alpha Roguelike/components/Fighter.py
+++ Alpha Roguelike/components/Fighter.py
@@ -1,7 +1,16 @@
+import tcod as libtcod
+
+from game_messages import Message
+
+
 class Fighter:
-    def __init__(self, hp, defense, power):
+    def __init__(self, hp, stamina, mana, defense, power):
         self.max_hp = hp
         self.hp = hp
+        self.max_stamina = stamina
+        self.stamina = stamina
+        self.max_mana = mana
+        self.mana = mana
         self.defense = defense
         self.power = power
 
@@ -21,11 +30,11 @@ class Fighter:
         damage = self.power - target.fighter.defense
 
         if damage > 0:
-            results.append({'message': '{0} attacks {1} for {2} hit points.'.format(
-                self.owner.name.capitalize(), target.name, str(damage))})
+            results.append({'message': Message('{0} attacks {1} for {2} hit points.'.format(
+                self.owner.name.capitalize(), target.name, str(damage)), libtcod.white)})
             results.extend(target.fighter.take_damage(damage))
         else:
-            results.append({'message': '{0} attacks {1} but does no damage.'.format(
-                self.owner.name.capitalize(), target.name)})
+            results.append({'message': Message('{0} attacks {1} but does no damage.'.format(
+                self.owner.name.capitalize(), target.name), libtcod.white)})
 
         return results
