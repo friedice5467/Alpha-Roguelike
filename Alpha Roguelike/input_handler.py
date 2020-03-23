805a60240d91c6d920404fa8513f0ec611c625d0
friedice <58054670+friedice5467@users.noreply.github.com>
2020-03-22 21:27:19 -0700

    Update input_handler.py
    3/22/2020
    added diagonal movement, added keypad support

--- Alpha Roguelike/input_handler.py
+++ Alpha Roguelike/input_handler.py
@@ -1,15 +1,26 @@
 import tcod as libtcod
 
 def handle_keys(key):
-    #movement keys
-    if key.vk == libtcod.KEY_UP:
+
+    #horizontal and vertical movement keys; ^ V < > in that order
+    if  key.vk == libtcod.KEY_UP or key.vk == libtcod.KEY_KP8:
         return {'move': (0, -1)}
-    elif key.vk == libtcod.KEY_DOWN:
+    elif key.vk == libtcod.KEY_DOWN or key.vk == libtcod.KEY_KP2:
         return {'move': (0, 1)}
-    elif key.vk == libtcod.KEY_LEFT:
+    elif key.vk == libtcod.KEY_LEFT or key.vk == libtcod.KEY_KP4:
         return {'move': (-1, 0)}
-    elif key.vk == libtcod.KEY_RIGHT:
+    elif key.vk == libtcod.KEY_RIGHT or key.vk == libtcod.KEY_KP6:
         return {'move': (1, 0)}
+
+    #diagonal movement keys
+    elif key.vk == libtcod.KEY_KP7:
+        return {'move': (-1, -1)}
+    elif key.vk == libtcod.KEY_KP9:
+        return {'move': (1, -1)}
+    elif key.vk == libtcod.KEY_KP1:
+        return {'move': (-1, 1)}
+    elif key.vk == libtcod.KEY_KP3:
+        return {'move': (1, 1)}
 
     if key.vk ==libtcod.KEY_ENTER and key.lalt:
         #alt+enter: toggle full screen
