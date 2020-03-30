292e12d4019bda7835840751b6cf340b7dc74158
friedice <58054670+friedice5467@users.noreply.github.com>
2020-03-30 11:56:13 -0700

    Update Engine.py
    3/30/2020
    cleaned up the code

--- Alpha Roguelike/Engine.py
+++ Alpha Roguelike/Engine.py
@@ -1,88 +1,42 @@
 import tcod as libtcod
 
 from components.ai import BasicMonster
-from components.fighter import Fighter
-from components.inventory import Inventory
-from map_objects.game_map import *
-from render_function import clear_all, render_all, RenderOrder
-from entity import Entity, get_blocking_entities_at_location
+from render_function import clear_all, render_all
+from entity import get_blocking_entities_at_location
 from game_states import GameStates
 from death_functions import kill_monster, kill_player
 from input_handler import handle_keys, handle_mouse
+from loader_functions.initialize_new_game import get_constants, get_game_variables
 from fov_functions import initialize_fov, recompute_fov
-from game_messages import Message, MessageLog
+from game_messages import Message
 
 
 def main():
-    #size of screen/map width/height for console initilization 
-    screen_width = 80
-    screen_height = 50
-    map_width = 80
-    map_height = 43
+    constants = get_constants()
 
-    #gui 
-    bar_width = 20
-    panel_height = 7
-    panel_y = screen_height - panel_height
-
-    message_x = bar_width + 2
-    message_width = screen_width - bar_width - 2
-    message_height = panel_height - 1
-
-    #defines the default room variables
-    room_max_size = 10
-    room_min_size = 6
-    max_rooms = 30
-
-    #defines the default fov variables
-    fov_algorithm = 0
-    fov_light_walls = True
-    fov_radius = 10
-    
-    #defines the max amount of stuff in a room
-    max_monsters_per_room = 3
-    max_items_per_room = 2
-
-    #defines colors
-    colors = {
-        'dark_wall': libtcod.Color(0, 0, 100),
-        'dark_ground': libtcod.Color(50, 50, 150),
-        'light_wall': libtcod.Color(130, 110, 50),
-        'light_ground': libtcod.Color(200, 180, 50)
-    }
-
-    #keeps track of position of player and other objects on the map on the map
-    fighter_component = Fighter(hp=30, stamina = 30, mana=10, defense=2, power=5)
-    inventory_component = Inventory(26)
-    player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True, render_order=RenderOrder.ACTOR,
-                    fighter=fighter_component, inventory=inventory_component)
-    entities = [player]
 
     #sets font 
     libtcod.console_set_custom_font(fontFile='arial12x12.png', flags=libtcod.FONT_TYPE_GRAYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
     #initializes the console
-    libtcod.console_init_root(w=screen_width, h=screen_height, title='libtcod alpha roguelike', fullscreen=False)
+    libtcod.console_init_root(constants['screen_width'], constants['screen_height'], constants['window_title'], False)
 
-    #defines width/height as a console
-    con = libtcod.console_new(w=screen_width, h=screen_height)
+    #defines a new console
+    con = libtcod.console_new(constants['screen_width'], constants['screen_height'])
 
-    #creates a new consolewhich holds hp bar and message log
-    panel = libtcod.console_new(screen_width, panel_height)
+    #defines a new panel
+    panel = libtcod.console_new(constants['screen_width'], constants['panel_height'])
     
-    game_map = GameMap(map_width, map_height)
-    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities, max_monsters_per_room, max_items_per_room)
+    #defines the entities and game map
+    player, entities, game_map, message_log, game_state = get_game_variables(constants)
 
     fov_recompute = True
 
     fov_map = initialize_fov(game_map)
 
-    message_log = MessageLog(message_x, message_width, message_height)
-
     key = libtcod.Key()
     mouse = libtcod.Mouse()
 
-    game_state = GameStates.PLAYERS_TURN
     previous_game_state = game_state
 
     targeting_item = None
@@ -94,11 +48,13 @@ def main():
 
         #checks for fov refresh and refreshes
         if fov_recompute:
-            recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
+            recompute_fov(fov_map, player.x, player.y, constants['fov_radius'], constants['fov_light_walls'],
+                          constants['fov_algorithm'])
 
         #initializes render_all function from render_functions
-        render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log, screen_width,
-                   screen_height, bar_width, panel_height, panel_y, mouse, colors, game_state)
+        render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log,
+                   constants['screen_width'], constants['screen_height'], constants['bar_width'],
+                   constants['panel_height'], constants['panel_y'], mouse, constants['colors'], game_state)
 
         fov_recompute = False
 
@@ -208,7 +164,7 @@ def main():
                 game_state = previous_game_state
 
                 message_log.add_message(Message('Targeting cancelled'))
-                
+
             if dead_entity:
 
                 if dead_entity == player:
