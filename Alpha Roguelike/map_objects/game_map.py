e18e8a96dee5c75dd757aecbf71e021ef05d2557
friedice <58054670+friedice5467@users.noreply.github.com>
2020-04-17 11:33:59 -0700

    Added Stairs

--- Alpha Roguelike/map_objects/game_map.py
+++ Alpha Roguelike/map_objects/game_map.py
@@ -3,6 +3,7 @@ import tcod as libtcod
 from components.ai import BasicMonster
 from components.fighter import Fighter
 from components.item import Item
+from components.stairs import Stairs
 from entity import Entity
 from game_messages import Message
 from item_functions import cast_fireball, cast_confuse, cast_lightning, heal
@@ -12,10 +13,12 @@ from map_objects.rectangle import Rect
 from random import randint
 
 class GameMap:
-    def __init__(self, width, height):
+    def __init__(self, width, height, dungeon_level=1):
         self.width = width
         self.height = height
         self.tiles = self.initialize_tiles()
+
+        self.dungeon_level = self.dungeon_level
 
     def initialize_tiles(self):
         tiles = [[Tile(True) for y in range(self.height)] for x in range(self.width)]
@@ -28,6 +31,9 @@ class GameMap:
                  max_monsters_per_room, max_items_per_room):
         rooms = []
         num_rooms = 0
+
+        center_of_last_room_x = None
+        center_of_last_room_y = None
 
         for r in range(max_rooms):
             #random width and height
@@ -52,6 +58,9 @@ class GameMap:
 
                 #center coordinates of new room
                 (new_x, new_y) = new_room.center()
+
+                center_of_last_room_x = new_x
+                center_of_last_room_y = new_y
 
                 if num_rooms == 0:
                     # this is the first room, where the player starts at
@@ -79,6 +88,11 @@ class GameMap:
                 #finally, append the new room to the list
                 rooms.append(new_room)
                 num_rooms += 1
+
+        stairs_component = Stairs(self.dungeon_level + 1)
+        down_stairs = Entity(center_of_last_room_x, center_of_last_room_y, '>', libtcod.white, 'Stairs',
+                             render_order=RenderOrder.STAIRS, stairs=stairs_component)
+        entities.append(down_stairs)
 
     def create_room(self, room):
         #go through the tiles in the rectangle and make them passable
