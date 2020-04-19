92a5fedee3804fcb6aa35952329c3b07c1027de6
friedice <58054670+friedice5467@users.noreply.github.com>
2020-04-18 19:27:00 -0700

    added levelup screen, char screen inputs

--- Alpha Roguelike/input_handler.py
+++ Alpha Roguelike/input_handler.py
@@ -13,6 +13,10 @@ def handle_keys(key, game_state):
         return handle_targeting_keys(key)
     elif game_state in (GameStates.SHOW_INVENTORY, GameStates.DROP_INVENTORY):
         return handle_inventory_keys(key)
+    elif game_state == GameStates.LEVEL_UP:
+        return handle_level_up_menu(key)
+    elif game_state == GameStates.CHARACTER_SCREEN:
+        return handle_character_screen(key)
 
     return {}
 
@@ -44,6 +48,10 @@ def handle_player_turn_keys(key):
     elif key.vk == libtcod.KEY_KP3:
         return {'move': (1, 1)}
 
+    #wait command
+    elif key.vk == libtcod.KEY_KP5:
+        return {'wait': True}
+
     #action commands
     if key_char == 'g':
         return {'pickup': True}
@@ -56,6 +64,9 @@ def handle_player_turn_keys(key):
 
     elif key.vk == libtcod.KEY_ENTER:
         return {'take_stairs': True}
+
+    elif key_char == 'c':
+        return {'show_character_screen': 'True'}
 
     if key.vk ==libtcod.KEY_ENTER and key.lalt:
         #alt+enter: toggle full screen
@@ -122,3 +133,25 @@ def handle_main_menu(key):
 
     return {}
 
+def handle_level_up_menu(key):
+    if key:
+        key_char = chr(key.c)
+
+        if key_char == 'a':
+            return {'level_up': 'hp'}
+        elif key_char == 'b':
+            return {'level_up': 'str'}
+        elif key_char == 'c':
+            return {'level_up': 'def'}
+        elif key_char == 'd':
+            return {'level_up': 'sp'}
+        elif key_char == 'e':
+            return {'level_up': 'mp'}
+
+    return {}
+
+def handle_character_screen(key):
+    if key.vk == libtcod.KEY_ESCAPE:
+        return {'exit': True}
+
+    return {}
